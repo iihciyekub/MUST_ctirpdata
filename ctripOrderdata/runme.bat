332b03481c148caa4db04a3c75df1562de96fea4
YjL <lyj8512@126.com>
2019-03-16 17:21:30 +0800

    删除文件夹,整理归类文件夹

--- ctripOrderdata/runme.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-start C:\Users\Administrator\AppData\Local\Programs\Python\Python37\pythonw.exe "C:\Users\Administrator\AppData\Local\Programs\Python\Python37\Lib\idlelib\idle.pyw" isruning.py
-
-
-
-exit
--- ctripOrderdata/start.bat
+++
@@ -1,3 +0,0 @@
-tasklist /nh|find /i "python.exe"
-if ERRORLEVEL 1 (echo ²»´æÔÚ) else taskkill /im python.exe /f
-python domain.py
